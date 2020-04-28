f3d1933ef0358964999f4b8ed7b139ee0cebdcf9
LMLittleMonk <1029513858@qq.com>
2020-04-28 19:21:47 +0800

    xadmin配置

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# class UserProfileAdmin(admin.ModelAdmin):
+#     pass
+# admin.site.register(UserProfile, UserProfileAdmin)
+from django.contrib.auth.admin import UserAdmin
 from apps.users.models import UserProfile
-class UserProfileAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(UserProfile,UserAdmin)
