5d1bb5b07ce25722267f00a66eb2709727acd9f0
LMLittleMonk <1029513858@qq.com>
2020-05-01 08:34:25 +0800

    index和login界面

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.views.generic.base import View
 # Create your views here.
+from apps.users.form import *
+from django.urls import reverse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib.auth import authenticate,login
+
+def IndexView(request):
+    return render(request,'index.html')
+
+class LoginView(View):
+    def get(slef,request,*args,**kwargs):
+        return render(request,'login.html')
+    def post(self,request,*args,**kwargs):
+        loginform = LoginForm(request.POST)
+        if loginform.is_valid():
+            username = loginform.cleaned_data["username"]
+            password = loginform.cleaned_data["password"]
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return HttpResponseRedirect(reverse("index"))
+            else:
+                return render(request,'login.html',{'msg':'用户名或密码错误','loginform':loginform})
+        else:
+            return render(request,'login.html',{'loginform':loginform})
